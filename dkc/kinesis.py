9c6b0dae030b185f8c29068b040249b9f43144a9
Leonid Rashkovsky <leonid@inner-active.com>
2014-05-12 16:08:54 +0300

    Added add_children, is_parent to shard. Because you can not split a parent chard

--- dkc/kinesis.py
+++ dkc/kinesis.py
@@ -6,13 +6,16 @@ from dkc.logger import get_logger
 from config import get_global_option, get_logging_option
 import time
 
+
 @total_ordering
 class Shard(object):
     def __init__(self, shard):
-        self.shard_id = shard.get('ShardId')
+        self.shard_id = str(shard.get('ShardId'))
         self.starting_hash = long(shard.get('HashKeyRange').get('StartingHashKey'))
         self.ending_hash = long(shard.get('HashKeyRange').get('EndingHashKey'))
         self.hash_range = self.ending_hash - self.starting_hash
+        self.parent_shard_id = shard.get('ParentShardId')
+        self.children = set()
 
     def __repr__(self):
         return repr('<Shard>: %s' % self.shard_id)
@@ -27,13 +30,19 @@ class Shard(object):
             return NotImplemented
         return self.hash_range == other.hash_range
 
+    def add_child(self, child_shard):
+        self.children.add(child_shard)
+
+    def is_parent(self):
+        return bool(self.children)
+
     def split(self):
         """
          Split the shard to two. Do not actually splits the shards but calculates the ranges of new shards.
         """
         new_shard_hash_range = self.hash_range / 2
         start = self.starting_hash
-        return start + new_shard_hash_range + 1
+        return str(start + new_shard_hash_range + 1)
 
 
 class Stream(object):
@@ -80,25 +89,43 @@ class Stream(object):
 
             Based on that, we wait until the stream is ready.
         """
-        if self.get_status() == 'Active':
+
+        if shard.is_parent():
+            self.logger.error('Trying to split a parent shard')
+            return
+
+        if self.get_status() == 'ACTIVE':
             try:
                 self.__conn.split_shard(self.name, shard.shard_id, shard.split())
                 self.logger.debug('Splitted shard')
-            except boto.exception.LimitExceededException, e:
-                self.logger.debug('%s Could not split shard %s' % (e.reason, e.message))
+            except Exception as e:
+                self.logger.debug('Could not split shard: {}'.format(e))
         else:
+            self.logger.debug('Sleeping for 60 seconds. %s' % self.get_status())
             time.sleep(60)
 
     def merge_shards(self, shards):
-        if self.get_status() == 'Active':
+        if self.get_status() == 'ACTIVE':
             try:
-                self.__conn.merge_shards(self.name, **shards)
-            except boto.exception.LimitExceededException, e:
-                self.logger.debug('%s Could not merge shards %s' % (e.reason, e.message))
+                self.__conn.merge_shards(self.name, *shards)
+                self.logger.debug('Merged shards')
+            except Exception as e:
+                self.logger.debug('Could not merge shards: %s' % (e))
 
         else:
             self.logger.debug('Sleeping for 60 seconds. %s' % self.get_status())
             time.sleep(60)
+
+    def get_shard(self, shard_id):
+        for s in self:
+            if s.shard_id == shard_id:
+                return s
+
+    def update_shards(self):
+        for s in self:
+            if s.parent_shard_id:
+                parent_shard = self.get_shard(s.parent_shard_id)
+                parent_shard.add_child(s)
 
 
 class Kinesis(object):
@@ -117,14 +144,15 @@ class Kinesis(object):
     def get_adjacent_shard(self, shard):
         for s in self.stream:
             if s.starting_hash - 1 == shard.ending_hash:
-                return shard, s
+                return shard.shard_id, s.shard_id
             if s.ending_hash + 1 == shard.starting_hash:
-                return s, shard
+                return s.shard_id, shard.shard_id
         return
 
     def get_shards(self):
         for s in self.stream:
-            yield s
+            if not s.is_parent():
+                yield s
 
     def get_shards_count(self):
         return len(self.stream)
@@ -133,13 +161,14 @@ class Kinesis(object):
 class ConnectionException(Exception):
     "Connection to Kinesis failed"
 
-
-
-
 if __name__ == '__main__':
     stream = 'Test'
     kinesis = Kinesis(stream)
+    kinesis.stream.update_shards()
 
-    print kinesis.get_biggest_shard()
-    print kinesis.get_shards()
-    print kinesis.get_shards_count()
+    smallest_shard = kinesis.get_smallest_shard()
+    shards = kinesis.get_adjacent_shard(smallest_shard)
+
+    print kinesis.stream.merge_shards(shards)
+
+    # print kinesis.stream.split_shard(kinesis.get_biggest_shard())
